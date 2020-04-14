aaeff81998890c51ced6c42d30a234e076bd1ab7
slug109 <slug109@daum.net>
2020-04-14 12:08:52 +0900

    git-bash후 푸시

--- src/Hellogit.java
+++ src/Hellogit.java
@@ -1,3 +1,5 @@
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Hellogit
 {
@@ -5,7 +7,9 @@ public class Hellogit
 	public static void main(String[] args)
 	{
 	System.out.println("git test!! 여긴학원1");
-
+	static ServerSocket serverSocket = null;
+	static Socket socket = null;
+	//클라이언트 정보 저장을 위한 Map 컬렉션 정의
 	}
 
 }
